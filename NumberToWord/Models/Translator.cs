89116474e6d0d7e91306a970e3514efb25c95cc2
Misaki Koonce <misaki.koonce@gmail.com>
2019-12-21 10:23:46 -0800

    Add backend logic. Need to fix 10 - 20.

--- NumberToWord/Models/Translator.cs
+++ NumberToWord/Models/Translator.cs
@@ -6,30 +6,134 @@ namespace NumberToWord.Models
     {
         // Dict holds 1 - 9
         public static Dictionary<long, string> Dictionary1 = new Dictionary<long, string>(){
-            {1, "one"}, {2, "two"}, {3, "three"}, {4, "four"}, {5, "five"}, {6, "six"}, {7, "seven"}, {8, "eight"}, {9, "nine"}
+            {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"}, {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
         };
         // Dict holds 11 - 19
         public static Dictionary<long, string> Dictionary2 = new Dictionary<long, string>(){
-            {11, "eleven"}, {12, "twelve"}, {31, "thirteen"}, {14, "forteen"}, {15, "fifteen"}, {16, "sixteen"}, {17, "seventeen"}, {18, "eighteen"}, {19, "nineteen"}
+            {11, "Eleven"}, {12, "Twelve"}, {31, "Thirteen"}, {14, "Forteen"}, {15, "Fifteen"}, {16, "Sixteen"}, {17, "Seventeen"}, {18, "Eighteen"}, {19, "Nineteen"}
         };
         // Dict holds 10 - 90
         public static Dictionary<long, string> Dictionary3 = new Dictionary<long, string>(){
-            {10, "ten"}, {20, "twenty"}, {3, "thirty"}, {4, "forty"}, {5, "fifty"}, {6, "sixty"}, {7, "seventy"}, {8, "eighty"}, {9, "ninety"}
+            {10, "Ten"}, {20, "Twenty"}, {3, "Thirty"}, {4, "Forty"}, {5, "Fifty"}, {6, "Sixty"}, {7, "Seventy"}, {8, "Eighty"}, {9, "Ninety"}
         };
 
         // Dict holds numbers of digits (track how many digits the user input is. This is referenced when input is more than 3 digits)
         public static Dictionary<long, string> Dictionary4 = new Dictionary<long, string>(){
-            {100, "hundred"}, {1000, "thousand"}, {1000000, "million"}, {1000000000, "billion"}, {1000000000000, "trillion"}
+            {100, "Hundred"}, {1000, "Thousand"}, {1000000, "Million"}, {1000000000, "Billion"}, {1000000000000, "Trillion"}
         };
         public static List<string> NumberToWord = new List<string>();
-        public static string TranslateInWord(int userInput)
+        public static string TranslateInWord(long userInput)
         {
-            // List<string> numberInWord = new List<string>();
-            // char[] numArray = userInput.ToString().ToCharArray();
-
-
-
-            return "";
+            Stack<long> myStack = new Stack<long>();
+            char[] numArray = userInput.ToString().ToCharArray();
+            for (int i = numArray.Length - 1; i >= 0; i--)
+            {
+                myStack.Push(Convert.ToInt64(Char.GetNumericValue(numArray[i])));
+            }
+            int count = myStack.Count;
+            string result = "";
+            string word;
+            while(count > 0)
+            {
+                word = "";
+                long num = 0;
+                switch(count)
+                {
+                    case 15:
+                        num = myStack.Pop();
+                        Dictionary1.TryGetValue(num, out word);
+                        result += word + " " + "Hundred" + " ";
+                        break;
+                    case 14:
+                        num = myStack.Pop();
+                        Dictionary3.TryGetValue(num * 10, out word);
+                        result += word + " ";
+                        break;
+                    case 13:
+                        num = myStack.Pop();
+                        Dictionary1.TryGetValue(num, out word);
+                        result += word + " " + "Trillion" + " ";
+                        break;
+                    case 12:
+                        num = myStack.Pop();
+                        Dictionary1.TryGetValue(num, out word);
+                        result += word + " " + "Hundred" + " ";
+                        break;
+                    case 11:
+                        num = myStack.Pop();
+                        Dictionary3.TryGetValue(num * 10, out word);
+                        result += word;
+                        break;
+                    case 10:
+                        num = myStack.Pop();
+                        Dictionary1.TryGetValue(num, out word);
+                        result += word + " " + "Billion" + " ";
+                        break;
+                    case 9:
+                        num = myStack.Pop();
+                        Dictionary1.TryGetValue(num, out word);
+                        result +=  word + " " + "Hundred" + " ";
+                        break;
+                    case 8:
+                        num = myStack.Pop();
+                        Dictionary3.TryGetValue(num * 10, out word);
+                        result += word;
+                        break;
+                    case 7:
+                        num = myStack.Pop();
+                        Dictionary1.TryGetValue(num, out word);
+                        result += word + " " + "Million" + " ";
+                        break;
+                    case 6:
+                        num = myStack.Pop();
+                        Dictionary1.TryGetValue(num, out word);
+                        result += word + " " + "Hundred" + " ";
+                        break;
+                    case 5:
+                        num = myStack.Pop();
+                        Dictionary3.TryGetValue(num * 10, out word);
+                        result += word;
+                        break;
+                    case 4:
+                        num = myStack.Pop();
+                        Dictionary1.TryGetValue(num, out word);
+                        result += word + " " + "Thousand" + " ";
+                        break;
+                    case 3:
+                        num = myStack.Pop();
+                        Dictionary1.TryGetValue(num, out word);
+                        result += word + " " + "Hundred" + " ";
+                        break;
+                    case 2:
+                        num = myStack.Pop();
+                        if(num > 1)
+                        {
+                            Dictionary3.TryGetValue(num * 10, out word);
+                            result += word ;  
+                        }
+                        else if (num == 0)
+                        {}
+                        else if (num == 1 && myStack.Peek() == 0)
+                        {
+                            Dictionary3.TryGetValue(10, out word);
+                            result += word;
+                        }
+                        else
+                        {
+                            num = myStack.Pop();
+                            Dictionary2.TryGetValue(num + 10, out word);
+                            result += word;
+                        }
+                        break;
+                    case 1:
+                        num = myStack.Pop();
+                        Dictionary1.TryGetValue(num, out word);
+                        result += word;
+                        break;
+                }
+                count = myStack.Count;
+            }
+            return result;
         }
     }
 }
