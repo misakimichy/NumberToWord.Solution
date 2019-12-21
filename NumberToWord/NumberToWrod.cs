359eb17c6b569d92e59f8d9611d011b3fa7cd9d7
Misaki Koonce <misaki.koonce@gmail.com>
2019-12-21 10:23:21 -0800

    Add frontend logic.

--- NumberToWord/NumberToWrod.cs
+++ NumberToWord/NumberToWrod.cs
@@ -6,7 +6,39 @@ namespace NumberToWord.Models
     {
         public static void Main()
         {
-            
+            Console.WriteLine("Welcome to Number to Words!");
+            StartGame();
+        }
+
+        private static void StartGame()
+        {
+            Console.WriteLine("Enter a number to translate in words");
+            long input = long.Parse(Console.ReadLine());
+            Console.WriteLine(Translator.TranslateInWord(input));
+            RepeatGame();
+        }
+
+        private static void RepeatGame()
+        {
+            Console.WriteLine("Do you want to play again? (Yes / No)");
+            int number;
+            string input = Console.ReadLine().ToLower();
+            bool isValid = Int32.TryParse(input, out number);
+            if (!isValid && input == "yes")
+            {
+                StartGame();
+            }
+            else if(!isValid && input == "no")
+            {
+                Console.WriteLine("See you next time!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid input!\n");
+                Console.ResetColor();
+                RepeatGame();
+            }
         }
     }
 }
